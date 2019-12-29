f8ffa2dac8d3a4da97ecfad23d877303db00b025
rchen03 <rex.chen@verizonmedia.com>
2019-12-29 17:08:39 +0800

    use safari process

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-var DEFAULT_TIMEOUT = 3000;
-
+var DEFAULT_TIMEOUT = 2000;
 var settings = {
     appStore: null,
     googlePlay: null,
@@ -19,51 +18,53 @@ var envs = {
     visibilityChange: null,
     isSafari: false,
     isChrome: false
-}
+};
 
 function handleVisibilityChange(e) {
-    if (document[envs.hidden]) {
+    if (!envs.isSafari && document[envs.hidden]) {
         clearTimeout(envs.timer);
     }
 }
 
 function checkDeviceOs() {
-    var ua = navigator.userAgent.toLowerCase()
-    var isIPhone = envs.testRegex.test(ua)
-    var isAndroid = !!~ua.indexOf('android')
-    
+    var ua = navigator.userAgent.toLowerCase();
+    var isIPhone = envs.testRegex.test(ua);
+    var isAndroid = !!ua.indexOf('android');
+
     envs.device = isIPhone ? envs.wording.ios : isAndroid ? envs.wording.android : envs.wording.ios;
 
-    if (envs.hidden !== "msHidden") {
-        if (navigator.bluetooth) {
+    if (envs.hidden !== 'msHidden') {
+        if (navigator.vendor.indexOf('Google') !== -1) {
             envs.isChrome = true;
         } else {
-            envs.isSafari = true;
+            if (navigator.userAgent.indexOf('CriOS') !== -1) {
+                envs.isChrome = true;
+            } else {
+                envs.isSafari = true;
+            }
         }
     }
 }
 
 function setEnv() {
-    if (typeof document.hidden !== "undefined") {
-        envs.hidden = "hidden";
-        envs.visibilityChange = "visibilitychange";
-    } else if (typeof document.msHidden !== "undefined") {
-        envs.hidden = "msHidden";
-        envs.visibilityChange = "msvisibilitychange";
-    } else if (typeof document.webkitHidden !== "undefined") {
-        envs.hidden = "webkitHidden";
-        envs.visibilityChange = "webkitvisibilitychange";
+    if (typeof document.hidden !== 'undefined') {
+        envs.hidden = 'hidden';
+        envs.visibilityChange = 'visibilitychange';
+    } else if (typeof document.msHidden !== 'undefined') {
+        envs.hidden = 'msHidden';
+        envs.visibilityChange = 'msvisibilitychange';
+    } else if (typeof document.webkitHidden !== 'undefined') {
+        envs.hidden = 'webkitHidden';
+        envs.visibilityChange = 'webkitvisibilitychange';
     }
 
     document.addEventListener(envs.visibilityChange, handleVisibilityChange, false);
 }
 
-exports.init = function(options) {
+exports.init = function(options = {}) {
     if (envs.isInit) {
         return;
     }
-
-    options = options || {};
 
     envs.isInit = true;
     settings.appStore = options.appStore || null;
@@ -73,44 +74,80 @@ exports.init = function(options) {
     try {
         setEnv();
         checkDeviceOs();
-    } catch(e) {
+    } catch (e) {
         console.log(e);
     }
 };
 
-exports.open = function(deeplink, customTimeout, fallbackAction) {
-    if (envs.isSafari) {
-        window.open(deeplink, 'deeplink');
-        return;
+exports.destroy = function() {
+    envs.isInit = false;
+    document.removeEventListener(envs.visibilityChange, handleVisibilityChange, false);
+};
+
+function makeFallbackUrl(fallbackAction = '') {
+    let fallbackUrl = fallbackAction;
+
+    // 如果後續動作是文字，表示應該有自己要去的位置
+    if (typeof fallbackAction === 'function') {
+        // 如果後續不是文字，那有可能有自己想做的動作，例如發送 beacon
+        fallbackAction(url => {
+            fallbackUrl = url;
+        });
     }
 
-    if (fallbackAction && customTimeout) {
-        var timeout = customTimeout || settings.timeout || DEFAULT_TIMEOUT;
-
-        clearTimeout(envs.timer);
-
-        envs.timer = setTimeout(function() {
-            if (typeof fallbackAction === 'string') {
-                var fallbackUrl = fallbackAction;
-
-                if (!fallbackUrl) {
-                    if (envs.device === envs.wording.ios && settings.appStore) {
-                        fallbackUrl = settings.appStore;
-                    } else if (envs.device === envs.wording.android && settings.googlePlay) {
-                        fallbackUrl = settings.googlePlay;
-                    }
-                }
-
-                if (fallbackUrl) {
-                    window.location.href = fallbackUrl;
-                }
-
-                return;
-            }
-            
-            fallbackAction && fallbackAction();
-        }, timeout);
+    // 預設值
+    if (!fallbackUrl && envs.device === envs.wording.ios && settings.appStore) {
+        fallbackUrl = settings.appStore;
+    } else if (!fallbackUrl && envs.device === envs.wording.android && settings.googlePlay) {
+        fallbackUrl = settings.googlePlay;
     }
 
-    window.location.href = deeplink;
+    return fallbackUrl;
 }
+
+// set timer
+function postProcess(target, failTimeout, fallbackAction, callback) {
+    envs.timer = setTimeout(function() {
+        var fallbackUrl = makeFallbackUrl(fallbackAction);
+        if (fallbackUrl) {
+            target.location.href = fallbackUrl;
+        }
+        callback && callback(fallbackUrl);
+    }, failTimeout);
+}
+
+// for safari
+function safariProcess(deeplink, timeout, fallbackAction) {
+    // 這傢伙話很多，所以要多一點時間給使用者
+    var failTimeout = timeout * 3;
+    // 如果是 safari, 直接 window open 開啟 deep link
+    var windowDeeplink = window.open(deeplink, 'deeplink');
+    // 後續 timer
+    postProcess(windowDeeplink, failTimeout, fallbackAction, fallbackUrl => {
+        if (fallbackUrl) {
+            setTimeout(function() {
+                windowDeeplink && windowDeeplink.close();
+            }, failTimeout);
+        } else {
+            windowDeeplink && windowDeeplink.close();
+        }
+    });
+}
+
+// for chrome
+function normalProcess(deeplink, timeout, fallbackAction) {
+    // 開啟 deep link
+    window.location.href = deeplink;
+    // 後續 timer
+    postProcess(window, timeout, fallbackAction);
+}
+
+exports.open = function(deeplink, customTimeout, fallbackAction) {
+    var timeout = customTimeout || settings.timeout || DEFAULT_TIMEOUT;
+    // 分流
+    if (envs.isSafari) {
+        safariProcess(deeplink, timeout, fallbackAction);
+    } else {
+        normalProcess(deeplink, timeout, fallbackAction);
+    }
+};
